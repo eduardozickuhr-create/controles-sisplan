a4cdfaa543723f4e8a6ac261bd18d44e921a20d9
eduardozickuhr-create <eduardozickuhr@gmail.com>
2026-08-23 20:29:05 -0300

    Atualiza service worker para V2

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-
-const CACHE='controles-sisplan-v1';
-const ASSETS=['./','index.html','styles.css','app.js','manifest.webmanifest','icon.svg','assets/FichaTecnica.fr3'];
+const CACHE='controles-sisplan-v2';
+const ASSETS=['./','index.html','styles.css','app.js','clientes.js','manifest.webmanifest','icon.svg','assets/FichaTecnica.fr3'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
