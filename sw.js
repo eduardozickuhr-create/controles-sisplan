bd378958c1fecb7d7c3912e659ce63f0d193e588
eduardozickuhr-create <eduardozickuhr@gmail.com>
2026-08-23 20:34:20 -0300

    Atualiza cache da aplicação para V2

--- sw.js
+++ sw.js
@@ -1,5 +1,4 @@
-const CACHE='controles-sisplan-v2';
-const ASSETS=['./','index.html','styles.css','app.js','clientes.js','manifest.webmanifest','icon.svg','assets/FichaTecnica.fr3'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='controles-sisplan-v2.1';
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','index.html','styles.css','v2.css','client-loader.js','app-v2.js','manifest.webmanifest','icon.svg','assets/FichaTecnica.fr3']))) });
+self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x))))])));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{let c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request))));
